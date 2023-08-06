17ce57e6b619e246c440464bb3911d606dfda8e7
Patrick Schroen <ps@ufotechnologies.com>
2023-08-06 15:13:39 -0400

    OGL Vanilla TypeScript Template with readme example

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,53 @@
-let isWorldSaved: boolean = false;
+import { Renderer, Camera, Transform, Box, Program, Mesh } from 'ogl';
 
-export function saveTheWorld(): string {
-  if (isWorldSaved) {
-    return `Too late, world has already been saved`;
-  } else {
-    isWorldSaved = true;
-    return `Hurray, you just saved the world`;
-  }
+{
+    const renderer = new Renderer();
+    const gl = renderer.gl;
+    document.body.appendChild(gl.canvas);
+
+    const camera = new Camera(gl);
+    camera.position.z = 5;
+
+    function resize() {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.perspective({
+            aspect: gl.canvas.width / gl.canvas.height,
+        });
+    }
+    window.addEventListener('resize', resize, false);
+    resize();
+
+    const scene = new Transform();
+
+    const geometry = new Box(gl);
+
+    const program = new Program(gl, {
+        vertex: /* glsl */ `
+            attribute vec3 position;
+
+            uniform mat4 modelViewMatrix;
+            uniform mat4 projectionMatrix;
+
+            void main() {
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            }
+        `,
+        fragment: /* glsl */ `
+            void main() {
+                gl_FragColor = vec4(1.0);
+            }
+        `,
+    });
+
+    const mesh = new Mesh(gl, { geometry, program });
+    mesh.setParent(scene);
+
+    requestAnimationFrame(update);
+    function update(t: DOMHighResTimeStamp) {
+        requestAnimationFrame(update);
+
+        mesh.rotation.y -= 0.04;
+        mesh.rotation.x += 0.03;
+        renderer.render({ scene, camera });
+    }
 }
