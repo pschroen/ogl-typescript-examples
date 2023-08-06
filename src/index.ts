86c5f29d9c77f59fa277d52fb9bd7d2631715616
Patrick Schroen <ps@ufotechnologies.com>
2023-08-06 11:01:12 -0400

    Setting Up a Vanilla TypeScript Project https://dev.to/anyiamchimdia/setting-up-a-vanilla-typescript-project-4me1

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let isWorldSaved: boolean = false;
+
+export function saveTheWorld(): string {
+  if (isWorldSaved) {
+    return `Too late, world has already been saved`;
+  } else {
+    isWorldSaved = true;
+    return `Hurray, you just saved the world`;
+  }
+}
