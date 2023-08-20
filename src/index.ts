d9303627d50a9053153ae876836abb795b523076
Patrick Schroen <ps@ufotechnologies.com>
2023-08-20 16:30:11 -0400

    Add normal maps example

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,146 @@
-import { Renderer, Camera, Transform, Geometry, Texture, Program, Mesh, Orbit, Text } from 'ogl';
+import { Renderer, Camera, Transform, Texture, Program, Geometry, Mesh, Orbit } from 'ogl';
 
-const vertex = /* glsl */ `
-    attribute vec2 uv;
+const vertex100 = /* glsl */ `
     attribute vec3 position;
+    attribute vec2 uv;
+    attribute vec3 normal;
 
+    uniform mat3 normalMatrix;
+    uniform mat4 modelMatrix;
     uniform mat4 modelViewMatrix;
     uniform mat4 projectionMatrix;
 
     varying vec2 vUv;
+    varying vec3 vNormal;
+    varying vec3 vMPos;
 
     void main() {
         vUv = uv;
+        vNormal = normalize(normalMatrix * normal);
+        vMPos = (modelMatrix * vec4(position, 1.0)).xyz;
 
         gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
     }
 `;
 
-const fragment = /* glsl */ `
-    uniform sampler2D tMap;
+const fragment100 = /* glsl */ `
+    #extension GL_OES_standard_derivatives : enable
+
+    precision highp float;
+
+    uniform mat4 viewMatrix;
+    uniform float uTime;
+    uniform sampler2D tDiffuse;
+
+    uniform sampler2D tNormal;
+    uniform float uNormalScale;
+    uniform float uNormalUVScale;
 
     varying vec2 vUv;
+    varying vec3 vNormal;
+    varying vec3 vMPos;
+
+    vec3 getNormal() {
+        vec3 pos_dx = dFdx(vMPos.xyz);
+        vec3 pos_dy = dFdy(vMPos.xyz);
+        vec2 tex_dx = dFdx(vUv);
+        vec2 tex_dy = dFdy(vUv);
+
+        vec3 t = normalize(pos_dx * tex_dy.t - pos_dy * tex_dx.t);
+        vec3 b = normalize(-pos_dx * tex_dy.s + pos_dy * tex_dx.s);
+        mat3 tbn = mat3(t, b, normalize(vNormal));
+
+        vec3 n = texture2D(tNormal, vUv * uNormalUVScale).rgb * 2.0 - 1.0;
+        n.xy *= uNormalScale;
+        vec3 normal = normalize(tbn * n);
+
+        // Get world normal from view normal
+        return normalize((vec4(normal, 0.0) * viewMatrix).xyz);
+    }
 
     void main() {
-        vec3 tex = texture2D(tMap, vUv).rgb;
-        float signedDist = max(min(tex.r, tex.g), min(max(tex.r, tex.g), tex.b)) - 0.5;
-        float d = fwidth(signedDist);
-        float alpha = smoothstep(-d, d, signedDist);
+        vec3 tex = texture2D(tDiffuse, vUv).rgb;
 
-        if (alpha < 0.01) discard;
+        vec3 normal = getNormal();
 
-        gl_FragColor.rgb = vec3(0.0);
-        gl_FragColor.a = alpha;
+        vec3 light = normalize(vec3(sin(uTime), 1.0, cos(uTime)));
+        float shading = dot(normal, light) * 0.5;
+
+        gl_FragColor.rgb = tex + shading;
+        gl_FragColor.a = 1.0;
     }
 `;
 
-const vertex100 =
-    /* glsl */ `
-` + vertex;
+const vertex300 = /* glsl */ `#version 300 es
+    in vec3 position;
+    in vec2 uv;
+    in vec3 normal;
 
-const fragment100 =
-    /* glsl */ `#extension GL_OES_standard_derivatives : enable
+    uniform mat3 normalMatrix;
+    uniform mat4 modelMatrix;
+    uniform mat4 modelViewMatrix;
+    uniform mat4 projectionMatrix;
+
+    out vec2 vUv;
+    out vec3 vNormal;
+    out vec3 vMPos;
+
+    void main() {
+        vUv = uv;
+        vNormal = normalize(normalMatrix * normal);
+        vMPos = (modelMatrix * vec4(position, 1.0)).xyz;
+
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+`;
+
+const fragment300 = /* glsl */ `#version 300 es
     precision highp float;
-` + fragment;
 
-const vertex300 =
-    /* glsl */ `#version 300 es
-    #define attribute in
-    #define varying out
-` + vertex;
+    uniform mat4 viewMatrix;
+    uniform float uTime;
+    uniform sampler2D tDiffuse;
 
-const fragment300 =
-    /* glsl */ `#version 300 es
-    precision highp float;
-    #define varying in
-    #define texture2D texture
-    #define gl_FragColor FragColor
+    uniform sampler2D tNormal;
+    uniform float uNormalScale;
+    uniform float uNormalUVScale;
+
+    in vec2 vUv;
+    in vec3 vNormal;
+    in vec3 vMPos;
+
     out vec4 FragColor;
-` + fragment;
+
+    vec3 getNormal() {
+        vec3 pos_dx = dFdx(vMPos.xyz);
+        vec3 pos_dy = dFdy(vMPos.xyz);
+        vec2 tex_dx = dFdx(vUv);
+        vec2 tex_dy = dFdy(vUv);
+
+        vec3 t = normalize(pos_dx * tex_dy.t - pos_dy * tex_dx.t);
+        vec3 b = normalize(-pos_dx * tex_dy.s + pos_dy * tex_dx.s);
+        mat3 tbn = mat3(t, b, normalize(vNormal));
+
+        vec3 n = texture(tNormal, vUv * uNormalUVScale).rgb * 2.0 - 1.0;
+        n.xy *= uNormalScale;
+        vec3 normal = normalize(tbn * n);
+
+        // Get world normal from view normal
+        return normalize((vec4(normal, 0.0) * viewMatrix).xyz);
+    }
+
+    void main() {
+        vec3 tex = texture(tDiffuse, vUv).rgb;
+
+        vec3 normal = getNormal();
+
+        vec3 light = normalize(vec3(sin(uTime), 1.0, cos(uTime)));
+        float shading = dot(normal, light) * 0.5;
+
+        FragColor.rgb = tex + shading;
+        FragColor.a = 1.0;
+    }
+`;
 
 {
     const renderer = new Renderer({ dpr: 2 });
@@ -65,7 +149,7 @@ const fragment300 =
     gl.clearColor(1, 1, 1, 1);
 
     const camera = new Camera(gl, { fov: 45 });
-    camera.position.set(0, 0, 7);
+    camera.position.set(1.5, 0, 2);
 
     const controls = new Orbit(camera);
 
@@ -78,73 +162,48 @@ const fragment300 =
 
     const scene = new Transform();
 
-    /*
+    const textureDiffuse = new Texture(gl);
+    const imgDiffuse = new Image();
+    imgDiffuse.onload = () => (textureDiffuse.image = imgDiffuse);
+    imgDiffuse.src = 'assets/granite-diffuse.jpg';
 
-    Instructions to generate necessary MSDF assets
-
-    Install msdf-bmfont https://github.com/soimy/msdf-bmfont-xml
-    `npm install msdf-bmfont-xml -g`
-
-    Then, using a font .ttf file, run the following (using 'FiraSans-Bold.ttf' as example)
-
-    `msdf-bmfont -f json -m 512,512 -d 2 --pot --smart-size FiraSans-Bold.ttf`
-
-    Outputs a .png bitmap spritesheet and a .json with character parameters.
-
-    */
-
-    const texture = new Texture(gl, {
-        generateMipmaps: false,
-    });
-    const img = new Image();
-    img.onload = () => (texture.image = img);
-    img.src = 'assets/fonts/FiraSans-Bold.png';
+    const textureNormal = new Texture(gl);
+    const imgNormal = new Image();
+    imgNormal.onload = () => (textureNormal.image = imgNormal);
+    imgNormal.src = 'assets/granite-normal.jpg';
 
     const program = new Program(gl, {
         // Get fallback shader for WebGL1 - needed for OES_standard_derivatives ext
         vertex: renderer.isWebgl2 ? vertex300 : vertex100,
         fragment: renderer.isWebgl2 ? fragment300 : fragment100,
         uniforms: {
-            tMap: { value: texture },
+            tDiffuse: { value: textureDiffuse },
+            tNormal: { value: textureNormal },
+            uNormalScale: { value: 2 },
+            uNormalUVScale: { value: 1 },
+            uTime: { value: 0 },
         },
-        transparent: true,
-        cullFace: false,
-        depthWrite: false,
     });
 
-    loadText();
-    async function loadText() {
-        const font = await (await fetch('assets/fonts/FiraSans-Bold.json')).json();
+    loadModel();
+    async function loadModel() {
+        const data = await (await fetch(`assets/rounded-cube.json`)).json();
 
-        const text = new Text({
-            font,
-            text: "don't panic",
-            width: 4,
-            align: 'center',
-            letterSpacing: -0.05,
-            size: 1,
-            lineHeight: 1.1,
-        });
-
-        // Pass the generated buffers into a geometry
         const geometry = new Geometry(gl, {
-            position: { size: 3, data: text.buffers.position },
-            uv: { size: 2, data: text.buffers.uv },
-            // id provides a per-character index, for effects that may require it
-            id: { size: 1, data: text.buffers.id },
-            index: { data: text.buffers.index },
+            position: { size: 3, data: new Float32Array(data.position) },
+            uv: { size: 2, data: new Float32Array(data.uv) },
+            normal: { size: 3, data: new Float32Array(data.normal) },
         });
 
         const mesh = new Mesh(gl, { geometry, program });
-
-        // Use the height value to position text vertically. Here it is centered.
-        mesh.position.y = text.height * 0.5;
         mesh.setParent(scene);
     }
 
     requestAnimationFrame(update);
     function update(t: DOMHighResTimeStamp) {
         requestAnimationFrame(update);
+
+        program.uniforms.uTime.value = t * 0.001;
 
         controls.update();
         renderer.render({ scene, camera });
